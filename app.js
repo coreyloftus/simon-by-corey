9247a75315f820dcc06ce7e87da50f29ad955f38
coreyloftus <coreyloftus@gmail.com>
2022-11-15 14:41:27 -0500

    Add Getters and first EventListener

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
 // global variables
-let cpuMoves = [1,2,3,4]
-let playerMoves = [1,2,3,4]
+let cpuMoves = [1,2]
+let playerMoves = [1,2]
+
 
 // getters & setters
+const cpuFirstTile = document.querySelector("#cpu-first-tile")
+const cpuSecondTile = document.querySelector("cpu-second-tile")
+const cpuThirdTile = document.querySelector("cpu-third-tile")
+const cpuFourthTile = document.querySelector("cpuFourthTile")
 
+const playerFirstTile = document.querySelector("#player-first-tile")
+const playerSecondTile = document.querySelector("#player-second-tile")
+const playerThirdTile = document.querySelector("#player-third-tile")
+const playerFourthTile = document.querySelector("#player-fourth-tile")
+
+cpuFirstTile.addEventListener('click', () => {
+    console.log(`${this} button clicked`)
+ })
+
+ 
 
 // testing
-gameRound()
+// gameRound()
 
 // functions
+
+
 function gameRound() {
-// computerTurn()
-// playerTurn()
-compareTurns()
+    // computerTurn()
+    // playerTurn()
+    compareTurns()
 }
 
+
 function computerTurn(){
+// notify user that it's the CPU's turn by unhiding CPU text
+// generate 4 random numbers and push them into the cpuMoves array
     for (let i=0;i<=3;i++) {
         cpuMoves.push(Math.floor(Math.random()*4)+1)
     }
@@ -25,22 +45,24 @@ function computerTurn(){
 }
 
 function playerTurn(){
-    for (let i=0;i<=3;i++) {
-        playerMoves.push(Math.floor(Math.random()*4)+1)
-    }
-    for (let i=0;i<=3;i++) {
-        console.log(`Player move ${i+1}: ${playerMoves[i]}`)
-    }
+// notify user that it's Player's turn by unhiding Player text
+    // get inputs based on which buttons are clicked by user
+//
 }
 
 function compareTurns(){
+// compare user moves to CPU's moves
     console.log(`Testing Moves now...`)
-    for (let i=0;i<=3;i++) {
+    for (let i=0;i<=cpuMoves.length-1;i++) {
         console.log(`Computer ${i+1}: ${cpuMoves[i]} | Player ${i+1}: ${playerMoves[i]}`)
-    if (cpuMoves[i] !== playerMoves[i]) {
+    if (cpuMoves[i] === playerMoves[i]) {
+        console.log(`Move ${i+1} matches`)
+        continue
+    } 
+    else {
         console.log(`Game Over`)
         break
-        }
-    }
-    console.log(`You matched them all!`)
+        } 
+    } console.log(`You win!`)
+    console.log(`End of compareTurns function.`)
 }
