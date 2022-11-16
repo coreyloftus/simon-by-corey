c9aaf611dbd93752d6edb53c83be068c5c78a889
coreyloftus <coreyloftus@gmail.com>
2022-11-16 12:28:26 -0500

    Add time delay between CPU turn and Player turn based on how many moves the CPU makes in the round.

--- app.js
+++ app.js
@@ -1,75 +1,58 @@
 // ##############################################
 // global variables
 // ##############################################
-let cpuMoves = [1,2,3,4]
-let playerMoves = []
+let cpuMoves = []
+let playerMoves = [1,2,3,4]
 const slowDown = (time) => {
     return new Promise((resolve) => setTimeout(resolve, time))
 }
-const displayCPUMoves2 = async (arr) => {
+const animateCPUMoves = async (arr) => {
+    console.log(`animating computer's moves now...`)
     for (let i = 0; i < arr.length; i++) {
         if (cpuMoves[i] === 1) {
-            console.log(`before slowDown`)
             await slowDown(2000)
             cpuFirstTile.classList.toggle('activate-tile')
             setTimeout(()=>{
                 cpuFirstTile.classList.toggle('activate-tile')
                 }, 1000)
-                console.log(`after slowDown`)
             } if (cpuMoves[i] === 2) {
                 await slowDown(2000)
                 cpuSecondTile.classList.toggle(`activate-tile`)
                 setTimeout(()=>{
                     cpuSecondTile.classList.toggle(`activate-tile`)
-                },1000*(i+1))
+                },1000)
             } if (cpuMoves[i] === 3) {
                 await slowDown(2000)
                 cpuThirdTile.classList.toggle(`activate-tile`)
                 setTimeout(()=>{
                     cpuThirdTile.classList.toggle(`activate-tile`)
-                },500)
+                },1000)
             } if (cpuMoves[i]=== 4) {
                 await slowDown(2000)
                 cpuFourthTile.classList.toggle(`activate-tile`)
                 setTimeout(()=>{
                     cpuFourthTile.classList.toggle(`activate-tile`)
-                },500)
+                },1000)
             } 
         }
+        console.log(`animateCPUMoves complete`)
+        await slowDown(1000)
+        console.log(`running slowDown for animateCPUMoves`)
+        await slowDown(1000)
+        console.log(`3...`)
+        await slowDown(1000)
+        console.log(`2...`)
+        await slowDown(1000)
+        console.log(`1...`)
+        await slowDown(1000)
     }
-displayCPUMoves2
-
-// function displayCPUMoves (arr) {
-//     for (let i = 0; i < arr.length; i++) {
-//     if (cpuMoves[i] === 1) {
-//             cpuFirstTile.classList.toggle('activate-tile')
-//             setTimeout(()=>{
-//                 cpuFirstTile.classList.toggle('activate-tile')
-//             }, 1000)
-//         } else if (cpuMoves[i] === 2) {
-//             cpuSecondTile.classList.toggle(`activate-tile`)
-//             setTimeout(()=>{
-//                 cpuSecondTile.classList.toggle(`activate-tile`)
-//             },1000*(i+1))
-//         } else if (cpuMoves[i] === 3) {
-//             cpuThirdTile.classList.toggle(`activate-tile`)
-//             setTimeout(()=>{
-//                 cpuThirdTile.classList.toggle(`activate-tile`)
-//             },500)
-//         } else if (cpuMoves[i]=== 4) {
-//             cpuFourthTile.classList.toggle(`activate-tile`)
-//             setTimeout(()=>{
-//                 cpuFourthTile.classList.toggle(`activate-tile`)
-//             },500)
-//         }
-//     }
-// }
+    
 
 // ##############################################
 // getters & setters
 // ##############################################
 
-// ##############################################
+
 // tiles
 // ##############################################
 const cpuFirstTile = document.querySelector("#cpu-first-tile")
@@ -82,73 +65,70 @@ const playerSecondTile = document.querySelector("#player-second-tile")
 const playerThirdTile = document.querySelector("#player-third-tile")
 const playerFourthTile = document.querySelector("#player-fourth-tile")
 
-// ##############################################
-// testing
-// ##############################################
-displayCPUMoves2(cpuMoves)
+// status bar text
+const cpuStatusText = document.querySelector("#cpu-status-text")
+const playerStatusText = document.querySelector("#player-status-text")
 
-// ##############################################
 // player tile listeners
-// ##############################################
-    playerFirstTile.addEventListener('click', () => {
+playerFirstTile.addEventListener('click', () => {
+    playerFirstTile.classList.toggle('activate-tile')
+    // removes activate-tile after animation completes
+    setTimeout(()=>{
         playerFirstTile.classList.toggle('activate-tile')
-        // removes activate-tile after animation completes
-        setTimeout(()=>{
-        	playerFirstTile.classList.toggle('activate-tile')
-        }, 500)
-        playerMoves.push(1)
-        console.log(`Player picked tile 1`)
-        console.log(`playerMoves array = ${playerMoves}`)
-        // check against computer move
-    })
-    playerSecondTile.addEventListener('click', () => {
+    }, 500)
+    playerMoves.push(1)
+    console.log(`Player picked tile 1`)
+    console.log(`playerMoves array = ${playerMoves}`)
+    // check against computer move
+})
+playerSecondTile.addEventListener('click', () => {
+    playerSecondTile.classList.toggle('activate-tile')
+    setTimeout(()=>{
         playerSecondTile.classList.toggle('activate-tile')
-        setTimeout(()=>{
-            playerSecondTile.classList.toggle('activate-tile')
-        }, 500)
-        playerMoves.push(2)
-        console.log(`Player picked tile 2`)
-        console.log(`playerMoves array = ${playerMoves}`)
-        // add to playerMove array
-        // check against computer move
-    })
-    playerThirdTile.addEventListener('click', () => {
+    }, 500)
+    playerMoves.push(2)
+    console.log(`Player picked tile 2`)
+    console.log(`playerMoves array = ${playerMoves}`)
+    // add to playerMove array
+    // check against computer move
+})
+playerThirdTile.addEventListener('click', () => {
+    playerThirdTile.classList.toggle('activate-tile')
+    setTimeout(()=>{
         playerThirdTile.classList.toggle('activate-tile')
-        setTimeout(()=>{
-            playerThirdTile.classList.toggle('activate-tile')
-        }, 500)
-        playerMoves.push(3)
-        console.log(`Player picked tile 3`)
-        console.log(`playerMoves array = ${playerMoves}`)
-        // add to playerMove array
-        // check against computer move
-    })
-    playerFourthTile.addEventListener('click', () => {
+    }, 500)
+    playerMoves.push(3)
+    console.log(`Player picked tile 3`)
+    console.log(`playerMoves array = ${playerMoves}`)
+    // add to playerMove array
+    // check against computer move
+})
+playerFourthTile.addEventListener('click', () => {
+    playerFourthTile.classList.toggle('activate-tile')
+    setTimeout(()=>{
         playerFourthTile.classList.toggle('activate-tile')
-        setTimeout(()=>{
-            playerFourthTile.classList.toggle('activate-tile')
-        }, 500)
-        playerMoves.push(4)
-        console.log(`Player picked tile 4`)
-        console.log(`playerMoves array = ${playerMoves}`)
-        // add to playerMove array
-        // check against computer move
-    })
-    
-    // status bar text
-    const cpuStatusText = document.querySelector("#cpu-status-text")
-    const playerStatusText = document.querySelector("#player-status-text")
+    }, 500)
+    playerMoves.push(4)
+    console.log(`Player picked tile 4`)
+    console.log(`playerMoves array = ${playerMoves}`)
+    // add to playerMove array
+    // check against computer move
+})
 
-    cpuStatusText.addEventListener('click', ()=>{
-        console.log('clicked on cpuStatusText')
-        cpuStatusText.classList.toggle('bold')
-    })
+cpuStatusText.addEventListener('click', ()=>{
+    console.log('clicked on cpuStatusText')
+    cpuStatusText.classList.toggle('bold')
+})
     
 // ##############################################
 // functions - working
 // ##############################################
 function computerTurn(){
-// notify user that it's the CPU's turn by unhiding CPU text
+// playerStatusText.classList.toggle("your-turn")
+// cpuStatusText.classList.toggle("your-turn")
+console.log(`computer's turn now.`)
+console.log(`generating random numbers for computer's moves`)
+
 // generate 4 random numbers and push them into the cpuMoves array
     for (let i=0;i<=3;i++) {
         cpuMoves.push(Math.floor(Math.random()*4)+1)
@@ -156,25 +136,31 @@ function computerTurn(){
     for (let i=0;i<=3;i++) {
         console.log(`Computer move ${i+1}: ${cpuMoves[i]}`)
     }
+    console.log(`end of computerTurn()`)
 }
+
+const playerTurn = async(computerMovesArr, playerMovesArr) => {
+// notify user that it's Player's turn by unhiding Player text
+console.log(`playerTurn starting`)
+console.log(`running slowDown for playerTurn`)
+await slowDown((computerMovesArr.length*4000))
+console.log(`3...`)
+await slowDown(1000)
+console.log(`2...`)
+await slowDown(1000)
+console.log(`1...`)
+await slowDown(1000)
+console.log(`Player turn should start now`)
+console.log(`playerMoves:${playerMovesArr}`)
+// cpuStatusText.classList.toggle(`your-turn`)
+// playerStatusText.classList.toggle(`your-turn`)
+}
+// get inputs based on which buttons are clicked by user
 
 
 // ##############################################
 // functions - NOT working
 // ##############################################
-
-// function gameRound() {
-//     computerTurn()
-//     playerTurn()
-//     compareTurns()
-// }
-
-
-// function playerTurn(){
-// notify user that it's Player's turn by unhiding Player text
-    // get inputs based on which buttons are clicked by user
-//
-// }
 
 // function compareTurns(){
 // compare user moves to CPU's moves
@@ -192,3 +178,21 @@ function computerTurn(){
 //     } console.log(`You win!`)
 //     console.log(`End of compareTurns function.`)
 // }
+
+// ##############################################
+// testing
+// ##############################################
+// function resolveAfter2Seconds(anyVar) {
+//     return new Promise (resolve => {
+//         setTimeout(()=>{
+//             console.log(`setTimeout between animate and playerturn`)
+//             resolve(anyVar)},
+//             ((cpuMoves.length)*2000))
+//     })
+// }
+
+
+computerTurn()
+animateCPUMoves(cpuMoves)
+// resolveAfter2Seconds()
+playerTurn(cpuMoves, playerMoves)
